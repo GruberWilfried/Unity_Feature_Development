9230d1aec88b42d73e595c27393f64513812143a
Will <wilfried.gruber@hotmail.com>
2019-12-05 18:58:38 +0100

    Add Feature: Create Prefab from Selection in Editor

---
+++ Assets/MyFeatures/CreatePrefabFromEditorSelection/CreatePrefab.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+class CreatePrefab
+{
+
+
+    [MenuItem("FeatureExtensions/Create Prefab from Selection")]
+    static void Create()
+    {
+        GameObject selection = Selection.activeGameObject;
+        string filePath = EditorUtility.SaveFilePanelInProject("Save Procedural Mesh", "pixel_mesh", "prefab", "");
+        if (filePath == "") return;
+
+        PrefabUtility.SaveAsPrefabAsset(selection, filePath);
+    }
+
+
+}
