b25f7c7e0222f790e0e2e3b2ff99a8d8ff77ca40
Will <wilfried.gruber@hotmail.com>
2019-12-07 14:53:33 +0100

    Add Feature: Object_Interaction_ActivateSubmeshes

---
+++ Assets/MyFeatures/Object_Interaction_ActivateSubmeshes/MakeChildrenDynamicOnTriggerEnter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeChildrenDynamicOnTriggerEnter : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        var rb = GetComponentsInChildren<Rigidbody>();
+        var mr = GetComponentsInChildren<BoxCollider>();
+
+        foreach (var item in rb)
+        {
+            item.isKinematic = false;
+            item.useGravity = true;
+        }
+
+        foreach (var item in mr)
+        {
+            item.enabled = true;
+        }
+    }
+}
