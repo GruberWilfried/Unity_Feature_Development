d056216310afdd2c7f2da4ae2860a18a63795527
Will <wilfried.gruber@hotmail.com>
2019-12-07 12:02:58 +0100

    Create Cubes from Editor Button with Mat assigned

--- Assets/MyFeatures/CreateMeshFromSprite/EditorCreateMeshFromSprite.cs
+++ Assets/MyFeatures/CreateMeshFromSprite/EditorCreateMeshFromSprite.cs
@@ -18,14 +18,14 @@ public class EditorCreateMeshFromSprite : EditorWindow
         GUILayout.Label("This is a label", EditorStyles.boldLabel);
 
         EditorGUILayout.BeginHorizontal();
-        source = EditorGUILayout.ObjectField(source, typeof(Sprite), true);
+        source = EditorGUILayout.ObjectField(source, typeof(Texture2D), true);
         EditorGUILayout.EndHorizontal();
 
         if (GUILayout.Button("Check"))
         {
             if (source != null)
             {
-
+                CreatePrefab(source as Texture2D, "test");
             }
         }
 
@@ -51,6 +51,44 @@ public class EditorCreateMeshFromSprite : EditorWindow
             AssetDatabase.CreateFolder(pathToFolder, pathElements[pathElements.Length-1]);
 
             Debug.Log(pathToFolder);
+        }
+    }
+
+    public void CreatePrefab(Texture2D tex, string name)
+    {
+        float width = tex.width;
+        float height = tex.height;
+
+        GameObject parent = new GameObject();
+        parent.name = name;
+
+        Material mat = new Material(Shader.Find("Standard"));
+        mat.mainTexture = tex as Texture2D;
+
+        // iterate through all pixels and create cubes
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                // get color of the individuel pixel
+                float red = tex.GetPixel(x, y).r;
+                float green = tex.GetPixel(x, y).g;
+                float blue = tex.GetPixel(x, y).b;
+                float alpha = tex.GetPixel(x, y).a;
+
+                if (alpha > 0.1f)
+                {
+                    Vector3 spawnPos = new Vector3(x,y,0);
+
+                    GameObject voxel = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+                    voxel.transform.position = spawnPos;
+
+                    voxel.transform.SetParent(parent.transform);
+
+                    voxel.GetComponent<MeshRenderer>().material = mat;
+                }
+            }
         }
     }
 }
