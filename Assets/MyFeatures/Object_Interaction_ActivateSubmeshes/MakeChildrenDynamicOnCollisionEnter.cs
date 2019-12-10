f99eec9bfcb4be707b9dff74d1f435e6a5042d22
GruberWilfried <wilfried.gruber@htl-salzburg.ac.at>
2019-12-10 17:15:26 +0100

    Change Script from Trigger to Collision and specify Tag of Collision Objects

---
+++ Assets/MyFeatures/Object_Interaction_ActivateSubmeshes/MakeChildrenDynamicOnCollisionEnter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeChildrenDynamicOnCollisionEnter : MonoBehaviour
+{
+    public string layerName = "ground";
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == layerName)
+        {
+            Debug.Log(other.gameObject.tag);
+            Debug.Log(other.gameObject.name);
+            var rb = GetComponentsInChildren<Rigidbody>();
+            var mr = GetComponentsInChildren<BoxCollider>();
+
+            foreach (var item in rb)
+            {
+                item.isKinematic = false;
+                item.useGravity = true;
+            }
+
+            foreach (var item in mr)
+            {
+                item.enabled = true;
+            }
+        }
+
+    }
+}
